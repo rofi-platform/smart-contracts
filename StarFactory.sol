b859b1b177276d48a13cddef8b2234c587be3e20
Tien Nguyen <mrtienhp97@gmail.com>
2021-09-02 10:51:26 +0700

    update star factory

--- StarFactory.sol
+++ StarFactory.sol
@@ -5,13 +5,13 @@ pragma solidity ^0.8.0;
 contract StarFactory {
     function getStarFromRandomness(uint256 _randomness) external pure returns(uint8) {
         uint seed = _randomness % 100;
-        if (seed < 60) {
+        if (seed < 65) {
             return 3;
         }
-        if (seed < 80) {
+        if (seed < 90) {
             return 4;
         }
-        if (seed < 95) {
+        if (seed < 98) {
             return 5;
         }
         return 6;
