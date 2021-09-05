db30a98740597862a5144a53a978e3e6af4c829d
DEntwickler <thangnguyenit1987@gmail.com>
2021-09-06 00:32:23 +0700

    fix NFT controller

--- controllers/CNFT.sol
+++ controllers/CNFT.sol
@@ -2,28 +2,36 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../interfaces/ICNFT";
 
-interface FactoryInterface {
-    function spawn(address sender) external;
-}
+import "../interfaces/INFT.sol";
 
-contract CNFT is ICNFT {
+contract CNFT is Ownable {
     IERC20 public paymentToken;
-    FactoryInterface public factoryContract;
+    INFT public nftContract;
 
     uint256 public eggPrice = 100000*10**18;
     address public deadAddress = 0x000000000000000000000000000000000000dEaD;
 
-    constructor(address paymentTokenAddress_, address factoryContractAddress_) {
-        factoryContract = FactoryInterface(factoryContractAddress_);
+    constructor(
+        address paymentTokenAddress_,
+        address nftContractAddress_
+    )
+    {
+        nftContract = INFT(nftContractAddress_);
         paymentToken = IERC20(paymentTokenAddress_);
+    }
+
+    function setBnbFee(uint bnbFee_) external onlyOwner {
+        nftContract.setBnbFee(bnbFee_);
     }
 
     function spawn() external payable {
         paymentToken.transferFrom(msg.sender, deadAddress, eggPrice);
-        factoryContract.spawn(msg.sender);
+        address random = nftContract.random();
+        random.call{value: msg.value}(new bytes(0));
+        nftContract.spawn(msg.sender);
     }
 
     function getStarFromRandomness(uint256 _randomness) external pure returns(uint8) {
