69a17a95eab577178b29d6b9512c635391dc321b
DEntwickler <thangnguyenit1987@gmail.com>
2021-09-06 01:32:38 +0700

    add testToken

---
+++ TestToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+
+    constructor () public ERC20("Token", "TKN") {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+}
