e99c22e5e77ed64afb7c79e7649feca0b27cdca8
DEntwickler <thangnguyenit1987@gmail.com>
2021-09-06 03:05:09 +0700

    fast deploy contract AIO

---
+++ Deployer.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./TestToken.sol";
+import "./Manager.sol";
+import "./NFT.sol";
+import "./controllers/CNFT.sol";
+contract Deployer {
+    // 0x0000000000000000000000000000000000000000
+    address constant public ZERO_ADDRESS = address(0x0);
+
+    TestToken public token;
+    Manager public manager;
+    NFT public nft;
+    CNFT public controller;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address paymentToken_
+    )
+    {
+        address sender = msg.sender;
+        token = TestToken(paymentToken_);
+        if (paymentToken_ == ZERO_ADDRESS) {
+            token = new TestToken();
+            token.transfer(sender, token.balanceOf(address(this)));
+        }
+
+        manager = new Manager();
+        manager.transferOwnership(sender);
+
+        nft = new NFT(name_, symbol_, address(manager));
+        nft.transferOwnership(sender);
+
+        controller = new CNFT(address(token), address(nft));
+        controller.transferOwnership(sender);
+    }
+}
--- TestToken.sol
+++ TestToken.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20 {
-
+contract TestToken is ERC20 {
     constructor () public ERC20("Token", "TKN") {
         _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
     }
