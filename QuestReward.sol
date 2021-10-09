5aeb6b0e7c2470bf2d56ce5044b6a1766f7fec15
Tien Nguyen <mrtienhp97@gmail.com>
2021-10-09 09:22:11 +0700

    QuestReward

--- QuestReward.sol
+++ QuestReward.sol
@@ -5,31 +5,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-interface IHero {
-	struct Hero {
-        uint8 star;
-        uint8 heroType;
-        bytes32 dna;
-        bool isGenesis;
-        uint256 bornAt;
-    }
-}
-
-interface IERC721 {
+interface INFT {
 	function ownerOf(uint256 tokenId) external view returns (address owner);
-	
-	function transferFrom(address from, address to, uint256 tokenId) external;
-}
-
-interface INFT is IERC721, IHero {
-	function getHero(uint256 _tokenId) external view returns (Hero memory);
 }
 
 interface IROFI {
     function transfer(address recipient, uint256 amount) external returns (bool);
 }
 
-contract QuestReward is IHero, Ownable {
+contract QuestReward is Ownable {
     INFT private nft;
     
     IROFI private rofi;
@@ -41,10 +25,11 @@ contract QuestReward is IHero, Ownable {
     event MerkleRootUpdated(bytes32 merkleRoot);
     
     struct Quest {
-        uint8 questId;
         uint8 mapToWin;
         uint256 reward;
     }
+    
+    uint8 latestQuestId = 0;
     
     mapping (uint8 => Quest) quests;
     
@@ -52,7 +37,7 @@ contract QuestReward is IHero, Ownable {
     
     event RewardClaim(uint256 heroId, uint8 questId, uint256 reward);
     
-    constructor(address _nft, address _cnft, address _rofi, address _payRofi) {
+    constructor(address _nft, address _rofi) {
         nft = INFT(_nft);
         rofi = IROFI(_rofi);
     }
@@ -61,13 +46,36 @@ contract QuestReward is IHero, Ownable {
         require(nft.ownerOf(_heroId) == _msgSender(), "not owner");
         require(!rewardClaimed[_heroId][_questId], "quest reward claimed");
         Quest memory quest = quests[_questId];
-        require(quest.mapToWin <= _mapCompleted, "not complete enough map");
+        require(_mapCompleted >= quest.mapToWin, "not complete enough map");
         bytes32 leaf = keccak256(abi.encodePacked(_heroId, _questId, _mapCompleted));
         require(MerkleProof.verify(_proof, merkleRoot, leaf), "proof not valid");
         uint256 reward = quest.reward;
-        rofi.transfer(_msgSender(), reward);
         rewardClaimed[_heroId][_questId] = true;
+        rofi.transfer(_msgSender(), reward);
         emit RewardClaim(_heroId, _questId, reward);
+    }
+    
+    function addQuest(uint8 _map, uint256 _reward) public onlyOwner {
+        uint8 questId = latestQuestId + 1;
+        quests[questId] = Quest({
+            mapToWin: _map,
+            reward: _reward
+        });
+        latestQuestId++;
+    }
+    
+    function getQuest(uint8 _questId) public view returns (Quest memory) {
+        return quests[_questId];
+    }
+    
+    function updateQuestReward(uint8 _questId, uint256 _reward) public onlyOwner {
+        Quest storage quest = quests[_questId];
+        quest.reward = _reward;
+    }
+    
+    function updateQuestMap(uint8 _questId, uint8 _mapToWin) public onlyOwner {
+        Quest storage quest = quests[_questId];
+        quest.mapToWin = _mapToWin;
     }
     
     function updateMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
