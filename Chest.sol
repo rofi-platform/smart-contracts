080790c8db9e9c1f6fe6493ab14723111316243b
Tien Nguyen <mrtienhp97@gmail.com>
2021-12-28 22:28:26 +0700

    remove Random-Test

--- Chest.sol
+++ Chest.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "./modules/NFT/Random-Test.sol";
+import "./modules/NFT/Random-Chest.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
