fcda7be9f24c013f8018b631307239115061e2b8
Tien Nguyen <mrtienhp97@gmail.com>
2022-02-17 23:45:04 +0700

    update farm

--- HeroFarm.sol
+++ HeroFarm.sol
@@ -45,19 +45,20 @@ contract HeroFarm is Ownable, IHero {
     struct Pool {
         IBEP20 rewardToken;
         uint8 starRequire;
+        uint256 totalHero;
         uint256 totalStar;
         uint256 rewardPerBlock;
         uint256 endBlock;
         uint256 lastRewardBlock;
-        uint256 accRofiPerShare;
+        uint256 accRewardPerShare;
         bool available;
     }
 
-    mapping (uint256 => Pool) public pools;
-    mapping (uint256 => mapping(address => Record)) public records;
+    mapping (uint256 => Pool) pools;
+    mapping (uint256 => mapping(address => Record)) records;
 
-    event Deposit(address indexed user, uint256 poolId, uint256 heroId);
-    event Withdraw(address indexed user, uint256 poolId, uint256 heroId);
+    event DepositHero(address indexed user, uint256 poolId, uint256 heroId);
+    event WithdrawHero(address indexed user, uint256 poolId, uint256 heroId);
     event CollectReward(address indexed user, uint256 poolId, uint256 amount);
 
     constructor(address _nft) {
@@ -74,18 +75,18 @@ contract HeroFarm is Ownable, IHero {
             pool.lastRewardBlock = block.number;
             return;
         }
-        uint256 rofiReward = getMultiplier(pool.lastRewardBlock, block.number, pool.endBlock).mul(pool.rewardPerBlock);
-        pool.accRofiPerShare = pool.accRofiPerShare.add(rofiReward.mul(1e12).div(totalStar));
+        uint256 totalReward = getMultiplier(pool.lastRewardBlock, block.number, pool.endBlock).mul(pool.rewardPerBlock);
+        pool.accRewardPerShare = pool.accRewardPerShare.add(totalReward.mul(1e12).div(totalStar));
         pool.lastRewardBlock = block.number;
     }
 
-    function deposit(uint256 _poolId, uint256 _heroId) public {
+    function depositHero(uint256 _poolId, uint256 _heroId) public {
         Pool storage pool = pools[_poolId];
         require(pool.available, "pool not available");
         Record storage record = records[_poolId][msg.sender];
         updatePool(_poolId);
         if (record.totalStar > 0) {
-            uint256 pending = record.totalStar.mul(pool.accRofiPerShare).div(1e12).sub(record.rewardDebt);
+            uint256 pending = record.totalStar.mul(pool.accRewardPerShare).div(1e12).sub(record.rewardDebt);
             if (pending > 0) {
                 pool.rewardToken.transfer(address(msg.sender), pending);
             }
@@ -96,18 +97,19 @@ contract HeroFarm is Ownable, IHero {
         nft.transferFrom(msg.sender, address(this), _heroId);
         record.totalStar = record.totalStar.add(hero.star);
         record.heroIds.push(_heroId);
-        record.rewardDebt = record.totalStar.mul(pool.accRofiPerShare).div(1e12);
+        record.rewardDebt = record.totalStar.mul(pool.accRewardPerShare).div(1e12);
+        pool.totalHero = pool.totalHero.add(1);
         pool.totalStar = pool.totalStar.add(hero.star);
-        emit Deposit(msg.sender, _poolId, _heroId);
+        emit DepositHero(msg.sender, _poolId, _heroId);
     }
 
-    function bulkDeposit(uint256 _poolId, uint256[] memory _heroIds) public {
+    function bulkDepositHero(uint256 _poolId, uint256[] memory _heroIds) public {
         for(uint256 i = 0; i < _heroIds.length; i++) {
-            deposit(_poolId, _heroIds[0]);
+            depositHero(_poolId, _heroIds[i]);
         }
     }
 
-    function withdraw(uint256 _poolId, uint256 _heroId) public {
+    function withdrawHero(uint256 _poolId, uint256 _heroId) public {
         Pool storage pool = pools[_poolId];
         require(pool.available, "pool not available");
         Record storage record = records[_poolId][msg.sender];
@@ -122,7 +124,7 @@ contract HeroFarm is Ownable, IHero {
         require(found == true, "not found hero");
         require(index >= 0, "not found index");
         updatePool(_poolId);
-        uint256 pending = record.totalStar.mul(pool.accRofiPerShare).div(1e12).sub(record.rewardDebt);
+        uint256 pending = record.totalStar.mul(pool.accRewardPerShare).div(1e12).sub(record.rewardDebt);
         if (pending > 0) {
             pool.rewardToken.transfer(address(msg.sender), pending);
         }
@@ -130,39 +132,40 @@ contract HeroFarm is Ownable, IHero {
         nft.transferFrom(address(this), msg.sender, _heroId);
         record.totalStar = record.totalStar.sub(hero.star);
         delete record.heroIds[index];
-        record.rewardDebt = record.totalStar.mul(pool.accRofiPerShare).div(1e12);
+        record.rewardDebt = record.totalStar.mul(pool.accRewardPerShare).div(1e12);
+        pool.totalHero = pool.totalHero.sub(1);
         pool.totalStar = pool.totalStar.sub(hero.star);
-        emit Withdraw(msg.sender, _poolId, _heroId);
+        emit WithdrawHero(msg.sender, _poolId, _heroId);
     }
 
-    function bulkWithdraw(uint256 _poolId, uint256[] memory _heroIds) public {
+    function bulkWithdrawHero(uint256 _poolId, uint256[] memory _heroIds) public {
         for(uint256 i = 0; i < _heroIds.length; i++) {
-            withdraw(_poolId, _heroIds[0]);
+            withdrawHero(_poolId, _heroIds[i]);
         }
     }
 
-    function pendingRofi(uint256 _poolId, address _user) external view returns (uint256) {
+    function claimableReward(uint256 _poolId, address _user) external view returns (uint256) {
         Pool storage pool = pools[_poolId];
         Record storage record = records[_poolId][_user];
-        uint256 accRofiPerShare = pool.accRofiPerShare;
+        uint256 accRewardPerShare = pool.accRewardPerShare;
         uint256 totalStar = pool.totalStar;
         if (block.number > pool.lastRewardBlock && totalStar != 0) {
             uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number, pool.endBlock);
-            uint256 rofiReward = multiplier.mul(pool.rewardPerBlock);
-            accRofiPerShare = accRofiPerShare.add(rofiReward.mul(1e12).div(totalStar));
+            uint256 totalReward = multiplier.mul(pool.rewardPerBlock);
+            accRewardPerShare = accRewardPerShare.add(totalReward.mul(1e12).div(totalStar));
         }
-        return record.totalStar.mul(accRofiPerShare).div(1e12).sub(record.rewardDebt);
+        return record.totalStar.mul(accRewardPerShare).div(1e12).sub(record.rewardDebt);
     }
 
-    function collectReward(uint256 _poolId) public {
+    function claimReward(uint256 _poolId) public {
         Pool storage pool = pools[_poolId];
         Record storage record = records[_poolId][msg.sender];
         updatePool(_poolId);
-        uint256 pending = record.totalStar.mul(pool.accRofiPerShare).div(1e12).sub(record.rewardDebt);
+        uint256 pending = record.totalStar.mul(pool.accRewardPerShare).div(1e12).sub(record.rewardDebt);
         if (pending > 0) {
             pool.rewardToken.transfer(address(msg.sender), pending);
         }
-        record.rewardDebt = record.totalStar.mul(pool.accRofiPerShare).div(1e12);
+        record.rewardDebt = record.totalStar.mul(pool.accRewardPerShare).div(1e12);
         emit CollectReward(msg.sender, _poolId, pending);
     }
 
@@ -184,20 +187,22 @@ contract HeroFarm is Ownable, IHero {
         uint256 _poolId,
         address _rewardToken,
         uint8 _starRequire,
+        uint256 _totalHero,
         uint256 _totalStar,
         uint256 _rewardPerBlock,
         uint256 _endBlock,
         uint256 _lastRewardBlock,
-        uint256 _accRofiPerShare
+        uint256 _accRewardPerShare
     ) public onlyOwner {
         pools[_poolId] = Pool({
             rewardToken: IBEP20(_rewardToken),
             starRequire: _starRequire,
+            totalHero: _totalHero,
             totalStar: _totalStar,
             rewardPerBlock: _rewardPerBlock,
             endBlock: _endBlock,
             lastRewardBlock: _lastRewardBlock,
-            accRofiPerShare: _accRofiPerShare,
+            accRewardPerShare: _accRewardPerShare,
             available: true
         });
     }
@@ -208,9 +213,17 @@ contract HeroFarm is Ownable, IHero {
         pool.rewardToken.transfer(msg.sender, pool.rewardToken.balanceOf(address(this)));
     }
 
-    function withdrawHero(uint256[] memory _heroIds) public onlyOwner {
+    function emergencyWithdraw(uint256[] memory _heroIds) public onlyOwner {
         for (uint256 i = 0; i < _heroIds.length; i++) {
             nft.transferFrom(address(this), msg.sender, _heroIds[i]);
         }
+    }
+
+    function getPool(uint256 _poolId) public view returns (Pool memory) {
+        return pools[_poolId];
+    }
+
+    function getRecord(uint256 _poolId, address _user) public view returns (Record memory) {
+        return records[_poolId][_user];
     }
 }
